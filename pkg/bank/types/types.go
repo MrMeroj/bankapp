539d4f6ff56797a9e1a6d2934fa4f685a6f5cf95
Supreme <merojsayfullo@gmail.com>
2021-12-12 11:50:18 +0500

    добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,34 @@
+package types
+
+//Money представляет собой денежную сумму в минимальныч единицах (центы, копейки, дирамы и т.д.) 
+type Money int64
+
+// Currency представляет код валюты
+type Currency string 
+
+// Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN представляет номер карты
+type PAN string
+
+// Card представляет информацию о платёжной карте
+type Card struct {
+	ID         int
+	PAN        string
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type Payment struct {
+	ID int 
+	Amount Money
+}
